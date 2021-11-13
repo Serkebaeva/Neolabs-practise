83864083f15ac3af51c4dda9fade7c927704ed91
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-11-13 23:40:20 +0600

    JS basics

---
+++ Lesson-5/js.js
@@ -0,0 +1,12 @@
+// ЦИКЛЫ
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+/*for (let i = 5; true; i++) {
+  console.log(i);
+}*/ //Из-за условия TRUE --> будут выводиться цифры БЕСКОНЕЧНО
+
+for (let i = 5; i <= 20; i += 5) {
+  console.log(i);
+}
