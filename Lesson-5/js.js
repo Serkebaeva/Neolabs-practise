a7c54a9aeaaac78bb088c1d7f5bb5b523827ab27
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-11-14 02:18:47 +0600

    assignments

--- Lesson-5/js.js
+++ Lesson-5/js.js
@@ -1,12 +1,20 @@
 // ЦИКЛЫ
-for (let i = 0; i < 5; i++) {
+/*for (let i = 0; i < 5; i++) {
   console.log(i);
-}
+}*/
 
 /*for (let i = 5; true; i++) {
   console.log(i);
 }*/ //Из-за условия TRUE --> будут выводиться цифры БЕСКОНЕЧНО
 
-for (let i = 5; i <= 20; i += 5) {
+/*for (let i = 5; i <= 20; i += 5) {
   console.log(i);
+}*/
+
+/*let names = ["Bakyt", "Erjan", "Gulzada", "Aizada", "Ibragim", "Bek"];
+
+for (let i = 0; i < names.length; i++) {
+  //  console.log("Hello, " + '"' + names[i] + '!"');
+  console.log(`Hello, ${names[i]}`);
 }
+*/
